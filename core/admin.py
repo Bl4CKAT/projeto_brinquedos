eea557e5498d8306281e6c4841a49bc9936cb38b
Joao Raphael <joaoraphael442@gmail.com>
2025-09-15 22:45:50 -0300

    Foi adicionado o app core nos INSTALLED_APPS

--- core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Brinquedo
-
-admin.site.register(Brinquedo)
-# Register your models here.
--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Brinquedo
-
-def lista_brinquedos(request):
-    brinquedos = Brinquedo.objects.all()
-    return render(request, "lista.html", {"brinquedos": brinquedos})
-
-# Create your views here.
