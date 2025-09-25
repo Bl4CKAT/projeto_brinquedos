f53f59ea972baab12fb568abbd36e906e5c574fa
Joao Raphael <joaoraphael442@gmail.com>
2025-09-25 01:12:36 -0300

    Foi feito o CRUD podendo adicionar brinquedos direto pelo site

--- brinquedos/core/urls.py
+++ brinquedos/core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_brinquedos, name='lista_brinquedos'),
+    path('editar/<int:id>/', views.editar_brinquedo, name='editar_brinquedo'),
+    path('excluir/<int:id>/', views.excluir_brinquedo, name='excluir_brinquedo'),
 ]
--- brinquedos/core/views.py
+++ brinquedos/core/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Brinquedo
+from .forms import BrinquedoForm
 
 def lista_brinquedos(request):
     brinquedos = Brinquedo.objects.all()
-    return render(request, "lista.html", {"brinquedos": brinquedos})
 
-# Create your views here.
+    if request.method == 'POST':
+        form = BrinquedoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_brinquedos')
+
+    else:
+        form = BrinquedoForm()
+
+    return render(request, "lista.html", {"brinquedos": brinquedos, "form": form})
+
+
+def editar_brinquedo(request, id):
+    brinquedo = get_object_or_404(Brinquedo, id=id)
+    if request.method == 'POST':
+        form = BrinquedoForm(request.POST, instance=Brinquedo)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_brinquedos')
+
+    else:
+        form = BrinquedoForm(instance=brinquedo)
+    return render(request, 'editar.html', {'form': form, 'brinquedo': brinquedo})
+
+
+def excluir_brinquedo(request, id):
+    brinquedo = get_object_or_404(Brinquedo, id=id)
+    if request.method == 'POST':
+        brinquedo.delete()
+        return redirect('lista_brinquedos')
+    return render(reuqest, 'exluir.html', {'brinquedo': brinquedo})
