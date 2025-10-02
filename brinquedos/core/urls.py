c6cbb424f7e6dc1700e618ae247aebef5fabc7e3
Joao Raphael <joaoraphael442@gmail.com>
2025-10-02 02:52:24 -0300

    Foi feito o pequeno prototipo da autenticação ainda falta arrumar mais coisas

--- brinquedos/core/urls.py
+++ brinquedos/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_brinquedos, name='lista_brinquedos'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.htm'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('editar/<int:id>/', views.editar_brinquedo, name='editar_brinquedo'),
     path('excluir/<int:id>/', views.excluir_brinquedo, name='excluir_brinquedo'),
