bed8384db5936b06cbde0d4307d76b92fc905efd
Joao Raphael <joaoraphael442@gmail.com>
2025-10-06 02:58:03 -0300

    Tentando adicionar pagina de logout

--- brinquedos/core/urls.py
+++ brinquedos/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_brinquedos, name='lista_brinquedos'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='logout.html'), name='logout'),
     path('editar/<int:id>/', views.editar_brinquedo, name='editar_brinquedo'),
     path('excluir/<int:id>/', views.excluir_brinquedo, name='excluir_brinquedo'),
 ]
